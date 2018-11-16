53795726b62afadc12608a2d5e714d899ecd6be6
FrankYeah <aaa24295234@gmail.com>
2018-11-16 22:26:59 +0800

    chart

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 let app = {};
 let numSelect = [];
 let historySelect = [];
+let chartSelect = [];
 
 // select lettery number
 let element= document.getElementsByClassName('circle');
@@ -12,7 +13,6 @@ let element= document.getElementsByClassName('circle');
         }else if(numSelect.length == 3 && historySelect.length<16){
             numSelect.push(element[i].textContent);
             element[i].style.background = `url("c-03.png") 0% 0% / cover no-repeat`;
-
         }
     });   
  }
@@ -21,7 +21,7 @@ let element= document.getElementsByClassName('circle');
  app.draw.addEventListener('click', function(){
 
     // draw on up 
-    if(numSelect.length == 4 && historySelect.length<16){
+    if(numSelect.length == 4){
         document.getElementById('rightUpPaddle').innerHTML = '';
         for(let i=0; i<4 ; i++){
             app.drawOn(numSelect[i], 'rightUpPaddle');
@@ -46,6 +46,7 @@ let element= document.getElementsByClassName('circle');
     if(historySelect.length>1){
         if(historySelect.length==6){
             historySelect.shift();
+            console.log(historySelect);
         }
         document.getElementById('rihtDownPaddle').innerHTML = '';
         for(let i=0; i<historySelect.length-1; i++){
@@ -54,8 +55,6 @@ let element= document.getElementsByClassName('circle');
             }
         }
     }
-
-
  })
 
  app.drawOn = function(text, append){
@@ -64,3 +63,30 @@ let element= document.getElementsByClassName('circle');
     newElement.textContent = text;
     document.getElementById(append).appendChild(newElement);
 }
+
+// chart 
+
+var myChart = echarts.init(document.getElementById('main'));
+
+// 指定图表的配置项和数据
+var option = {
+    title: {
+        text: 'Win rate'
+    },
+    tooltip: {},
+    legend: {
+        data:['lottery number']
+    },
+    xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子", '哈哈']
+    },
+    yAxis: {},
+    series: [{
+        name: 'lottery number',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20, 99]
+    }]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
