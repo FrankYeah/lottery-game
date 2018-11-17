6404f67b57e9eaa718d037f8bf2ba43f9c66b18e
FrankYeah <aaa24295234@gmail.com>
2018-11-17 14:58:54 +0800

    chart finish

--- home.js
+++ home.js
@@ -2,11 +2,15 @@ let app = {};
 let numSelect = [];
 let historySelect = [];
 let chartSelect = [];
+let chartAll = [];
+let selectValue =[];
+let selectCount =[];
 
 // select lettery number
 let element= document.getElementsByClassName('circle');
  for(let i=0; i<element.length; i++){
     element[i].addEventListener('click', function(){
+
         if(numSelect.length < 3 && historySelect.length<16){
             numSelect.push(element[i].textContent);
             element[i].style.background = `url("c-02.png") 0% 0% / cover no-repeat`;
@@ -14,16 +18,22 @@ let element= document.getElementsByClassName('circle');
             numSelect.push(element[i].textContent);
             element[i].style.background = `url("c-03.png") 0% 0% / cover no-repeat`;
         }
+
     });   
  }
 
  app.draw = document.getElementsByClassName('left-down-paddle')[0];
  app.draw.addEventListener('click', function(){
 
-    // draw on up 
+    // draw current on 
     if(numSelect.length == 4){
         document.getElementById('rightUpPaddle').innerHTML = '';
         for(let i=0; i<4 ; i++){
+
+            chartAll.push(Number(numSelect[i]));
+            if(chartAll.length>20){
+                chartAll.shift();
+            }
             app.drawOn(numSelect[i], 'rightUpPaddle');
             var select = document.querySelector('.right-up-paddle');
             var inner = select.querySelectorAll('.circle2');
@@ -46,7 +56,6 @@ let element= document.getElementsByClassName('circle');
     if(historySelect.length>1){
         if(historySelect.length==6){
             historySelect.shift();
-            console.log(historySelect);
         }
         document.getElementById('rihtDownPaddle').innerHTML = '';
         for(let i=0; i<historySelect.length-1; i++){
@@ -55,6 +64,8 @@ let element= document.getElementsByClassName('circle');
             }
         }
     }
+
+    app.drawChart();
  })
 
  app.drawOn = function(text, append){
@@ -65,29 +76,51 @@ let element= document.getElementsByClassName('circle');
 }
 
 // chart 
+app.drawChart = function(){
+    chartSelect = [];
+    chartSelect = chartSelect.concat(chartAll);
+    chartSelect.sort(function (a, b) {
+        return a - b;
+    });
 
-var myChart = echarts.init(document.getElementById('main'));
+    // get count
+    selectValue =[];
+    selectCount =[];
+    
+    for(let i=0 ; i<chartSelect.length; i++){
+      if(chartSelect[i] != chartSelect[i-1]){
+        selectValue.push(chartSelect[i]);
+        selectCount.push(1);
+        
+      }
+      if(i>0 && chartSelect[i] == chartSelect[i-1]){
+        selectCount[selectCount.length-1] = selectCount[selectCount.length-1] +1
+      }
+    }
 
-// 指定图表的配置项和数据
-var option = {
-    title: {
-        text: 'Win rate'
-    },
-    tooltip: {},
-    legend: {
-        data:['lottery number']
-    },
-    xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子", '哈哈']
-    },
-    yAxis: {},
-    series: [{
-        name: 'lottery number',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20, 99]
-    }]
-};
+    // draw chart
+    var myChart = echarts.init(document.getElementById('main'));
 
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
-
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: 'Win rate'
+        },
+        tooltip: {},
+        legend: {
+            data:['lottery number']
+        },
+        xAxis: {
+            data: selectValue
+        },
+        yAxis: {},
+        series: [{
+            name: 'lottery number',
+            type: 'bar',
+            data: selectCount
+        }]
+    };
+    
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+}
