fa7cd00a89d07c37899676bc75506b2beaba69a0
FrankYeah <aaa24295234@gmail.com>
2018-11-18 08:27:17 +0800

    another chart

--- home.js
+++ home.js
@@ -5,16 +5,18 @@ let chartSelect = [];
 let chartAll = [];
 let selectValue =[];
 let selectCount =[];
+let secondObj ={value:0,name:''};
+let secondChart = [];
 
 // select lettery number
 let element= document.getElementsByClassName('circle');
  for(let i=0; i<element.length; i++){
     element[i].addEventListener('click', function(){
 
-        if(numSelect.length < 3 && historySelect.length<16){
+        if(numSelect.length < 3 && historySelect.length<16 && element[i].textContent != numSelect[0] && element[i].textContent != numSelect[1]){
             numSelect.push(element[i].textContent);
             element[i].style.background = `url("c-02.png") 0% 0% / cover no-repeat`;
-        }else if(numSelect.length == 3 && historySelect.length<16){
+        }else if(numSelect.length == 3 && historySelect.length<16 && element[i].textContent != numSelect[0] && element[i].textContent != numSelect[1] && element[i].textContent != numSelect[2]){
             numSelect.push(element[i].textContent);
             element[i].style.background = `url("c-03.png") 0% 0% / cover no-repeat`;
         }
@@ -64,7 +66,6 @@ let element= document.getElementsByClassName('circle');
             }
         }
     }
-
     app.drawChart();
  })
 
@@ -91,14 +92,13 @@ app.drawChart = function(){
       if(chartSelect[i] != chartSelect[i-1]){
         selectValue.push(chartSelect[i]);
         selectCount.push(1);
-        
       }
       if(i>0 && chartSelect[i] == chartSelect[i-1]){
         selectCount[selectCount.length-1] = selectCount[selectCount.length-1] +1
       }
     }
 
-    // draw chart
+    // draw first chart 
     var myChart = echarts.init(document.getElementById('main'));
 
     // 指定图表的配置项和数据
@@ -123,4 +123,50 @@ app.drawChart = function(){
     
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
+
+//-----------------------------------------------------------------------------------------------------//
+    // draw second chart
+    var mainTwo = echarts.init(document.getElementById('mainTwo'));
+    secondChart =[];
+    for(let i=0; i<selectValue.length; i++){
+        secondObj.value = selectCount[i];
+        secondObj.name = selectValue[i];
+        secondChart.push(secondObj);
+        secondObj ={value:0,name:''};
+    }
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    mainTwo.setOption({
+        backgroundColor: '#2c343c',
+        series : [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: '55%',
+                data:secondChart,
+                roseType: 'angle',
+                label: {
+                    normal: {
+                        textStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        }
+                    }
+                },
+                labelLine: {
+                    normal: {
+                        lineStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        }
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        shadowBlur: 200,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }              
+            }
+        ]
+    })
 }
